e37b602bfb10b4213bdf569877d5284796caf7f1
tianheg <yidajiabei@163.com>
2021-06-08 15:47:00 +0800

    Disable open external link in new tab(Temporarily)

--- assets/js/external-link.js
+++ assets/js/external-link.js
@@ -1,11 +1,11 @@
 // Open external links in new tab
-function external_new_window() {
-    for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
-        var b = c[a];
-        if(b.getAttribute("href") ) {
-            b.target = "_blank";
-            b.rel = "noopener";
-        }
-    }
-}
-external_new_window();
+// function external_new_window() {
+//     for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
+//         var b = c[a];
+//         if(b.getAttribute("href") ) {
+//             b.target = "_blank";
+//             b.rel = "noopener";
+//         }
+//     }
+// }
+// external_new_window();
