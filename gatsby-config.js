c1c98abe4e99f0459f3a082c2604eb2e3d51d604
tianheg <yidajiabei@163.com>
2021-07-19 19:25:24 +0800

    add: code highlight

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
         rootNote: `/index`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-theme-ui',
+      options: {
+        prismPreset: 'duotone-dark', // night-owl,dracula,duotone-dark,duotone-light,github，night-owl-light，oceanic-next,prism-coy,prism-dark,prism-funky,prism-okaidia,prism-solarizedlight,prism-tomorrow,prism-twilight(didn't work),prism,shades-of-purple,vs-dark
+      },
+    },
   ],
   siteMetadata: {
     title: `Think Tech`,
